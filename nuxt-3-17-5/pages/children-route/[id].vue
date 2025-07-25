3764b6e6432b98bfd02861d9a8db9adf0d8ba8e4
yuechao.xu <yuechao.xu@appshahe.com>
2025-07-25 18:49:30 +0800

    feat: 优化页面性能和布局结构
    在default-alive.vue和keep-alive.vue中进行性能优化，确保与新的布局结构兼容。更新index.vue的页面挂载和卸载逻辑，提升用户体验。

---
+++ nuxt-3-17-5/pages/children-route/[id].vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>我是 children-route 页面</h1>
+        <NuxtPage></NuxtPage>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ nuxt-3-17-5/pages/children-route/[id]/children1.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>我是 children1 页面</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
