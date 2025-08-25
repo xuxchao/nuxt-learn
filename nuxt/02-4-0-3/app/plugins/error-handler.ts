99c61a37843354bad42dbb9d69da38844fa1472c
yuechao.xu <yuechao.xu@appshahe.com>
2025-08-25 14:00:41 +0800

    feat: 重命名文件夹。更符合后续规范

---
+++ nuxt/02-4-0-3/app/plugins/error-handler.ts
@@ -0,0 +1,16 @@
+export default defineNuxtPlugin((nuxtApp) => {
+  // vue 错误捕获
+  nuxtApp.vueApp.config.errorHandler = (error, context) => {
+    console.error("vueApp.config.errorHandler:", error, context);
+  };
+
+  // 应用启动错误，客户端，服务端渲染错误，交互错误
+  nuxtApp.hook("vue:error", (error) => {
+    console.error("vue:error:", error);
+  });
+
+  // 应用启动错误，客户端，服务端渲染错误
+  nuxtApp.hook("app:error", (error) => {
+    console.error("app:error:", error);
+  });
+});
--- nuxt/03-4-0-3-template/app/plugins/error-handler.ts
+++ nuxt/03-4-0-3-template/app/plugins/error-handler.ts
@@ -1,13 +1,15 @@
 export default defineNuxtPlugin((nuxtApp) => {
-  // 捕获客户端渲染错误
+  // vue 错误捕获：只在客户端生效
   nuxtApp.vueApp.config.errorHandler = (error, context) => {
     console.error("vueApp.config.errorHandler:", error, context);
   };
 
+  // 应用启动错误，客户端，服务端渲染错误，交互错误
   nuxtApp.hook("vue:error", (error) => {
     console.error("vue:error:", error);
   });
 
+  // 应用启动错误，客户端，服务端渲染错误
   nuxtApp.hook("app:error", (error) => {
     console.error("app:error:", error);
   });
