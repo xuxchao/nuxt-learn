e8d7c30b603e61991fcfc7bd04c885e76d65f4a7
yuechao.xu <yuechao.xu@appshahe.com>
2025-09-02 08:51:01 +0800

    feat(fetch): 添加fetch示例页面并修改API响应
    添加use-fetch和fetch组件页面，展示不同fetch方式的使用
    修改hello.ts API响应，添加延迟和计数功能
    修复use-async-data页面的错误处理和刷新按钮功能

--- nuxt/02-4-0-3/app/pages/fetch/components/fetch.vue
+++ nuxt/02-4-0-3/app/pages/fetch/components/fetch.vue
@@ -14,18 +14,7 @@
 
 // 加不加 await 服务端都会同步等待结果
 // 加了 await 客户端会等待接口完成在跳转页面
-const { data, refresh, pending,error } = await useAsyncData('use-async-data',  () => {
-    // await delay(5000)
-    // throw createError({
-    //     statusCode: 500,
-    //     message: '服务器错误1',
-    // })
-  return $fetch('/api/hello')
-}, {
-    // 不加 await 的情况没有任何区别
-    // 加了 await 的情况下，vue 的 Suspense 组件不会等待接口完成
-    lazy: true,
-})
+const { data, refresh, pending,error } = await useFetch('/api/hello')
 
 if(import.meta.server) {
     console.log('页面服务端渲染')
--- nuxt/02-4-0-3/server/api/hello.ts
+++ nuxt/02-4-0-3/server/api/hello.ts
@@ -1,14 +1,18 @@
+import { delay } from 'es-toolkit'
 
+
+let num = 1;
 
 export default defineEventHandler(async (_event) => {
-    console.log('开始')
-    // await delay(5000)
-    console.log('结束')
-    throw createError({
-        statusCode: 500,
-        message: '服务器错误',
-    })
+    // console.log('开始')
+    await delay(3000)
+    num++;
+    // console.log('结束')
+    // throw createError({
+    //     statusCode: 500,
+    //     message: '服务器错误',
+    // })
   return {
-    message: 'hello world',
+    message: 'hello world' + num,
   }
 })
